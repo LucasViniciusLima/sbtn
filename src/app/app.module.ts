4231ec0b8e444c0bed43bef15533c560aff164d6
Lucas_Vinícius_Lima_Bezerra <lucaslimavzt@gmail.com>
2021-06-21 12:00:30 -0300

    add and exclude items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/ferramentas/adm-atividades/adm-atividades.component.ts
+++ src/app/ferramentas/adm-atividades/adm-atividades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Atividade } from './atividade.model';
 
 
@@ -11,17 +12,71 @@ export class AdmAtividadesComponent implements OnInit {
 
 
   atividadesList: any[] = new Array();
-  
 
-  constructor() { }
+  atividade: Atividade = new Atividade('', '', 2, 2);
+
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
 
-  criarNovaAtividade(titulo: string, descricao: string, urgencia: number, importancia: number){
-    var atividade = new Atividade(titulo,descricao,urgencia,importancia);
-    this.atividadesList.push(atividade);
-    console.log(this.atividadesList);
+  deleteAtividade(numId:number){
+    this.atividadesList.splice(numId,1);
+  }
+
+  criarNovaAtividade() {
+    if (this.atividade.titulo != '') {
+      var atividade = new Atividade(this.atividade.titulo, this.atividade.descricao, this.atividade.urgencia, this.atividade.importancia);
+      this.atividadesList.push(atividade);
+      this.atividade = new Atividade('','',2,2);
+    }    
+  }
+
+  getNoToDo() {
+    var arr = new Array();
+
+    for (let i = 0; i < this.atividadesList.length; i++) {
+      if (this.atividadesList[i].urgencia <= 4 && this.atividadesList[i].importancia <= 4) arr.push({item: this.atividadesList[i], id: i});
+    }
+
+    return arr;
+  }
+
+  getDelegate(){
+    var arr = new Array();
+
+    for (let i = 0; i < this.atividadesList.length; i++) {
+      if (this.atividadesList[i].urgencia >= 5 && this.atividadesList[i].importancia <=4) arr.push({item: this.atividadesList[i], id: i});
+    }
+
+    return arr;
+  }
+
+  getDo(){
+    var arr = new Array();
+
+    for (let i = 0; i < this.atividadesList.length; i++) {
+      if (this.atividadesList[i].urgencia < 7 && this.atividadesList[i].importancia >= 5) arr.push({item: this.atividadesList[i], id: i});
+    }
+
+    return arr;
+  }
+
+  getDoNow(){
+    var arr = new Array();
+
+    for (let i = 0; i < this.atividadesList.length; i++) {
+      if (this.atividadesList[i].urgencia >= 7 && this.atividadesList[i].importancia >= 5) arr.push({item: this.atividadesList[i], id: i});
+    }
+
+    return arr;
+  }
+
+  deleteItem(id: number){
+    this.atividadesList.splice(id,1);
   }
 
 }
